3d3120ee83af3feb8afe7ef561223de1e4a0acab
Lucas Alexandre de Carvalho Silva <lucasfofs7@gmail.com>
2023-07-26 15:26:56 -0300

    Reconfiguração da branch Lucas

--- src/pages/Sobre copy/index.jsx
+++ src/pages/Sobre copy/index.jsx
@@ -1,14 +1,13 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import "./styles.css";
+import "./bootstrap/css/bootstrap.min.css";
+import "./bootstrap/js/bootstrap.bundle.min.js";
 
-import "../bootstrap/css/bootstrap.min.css";
-import "../bootstrap/js/bootstrap.bundle.min.js";
-//import "./stylesSobre.css";
+import { Link } from "react-router-dom";
 
 export function Sobre() {
   return (
     <>
-    <div className="sobre">
       <header className="header" id="header">
         <div className="header-logo">
           <svg
@@ -57,66 +56,42 @@ export function Sobre() {
               <div className="col-lg-6">
                 <h3>Lorem Ipsum</h3>
                 <p>
-                  Contrary to popular belief, Lorem Ipsum is not simply random
-                  text. It has roots in a piece of classical Latin literature
-                  from 45 BC, making it over 2000 years old. Richard McClintock,
-                  a Latin professor at Hampden-Sydney College in Virginia,
-                  looked up one of the more obscure Latin words, consectetur,
-                  from a Lorem Ipsum passage, and going through the cites of the
-                  word in classical literature, discovered the undoubtable
-                  source.
+                Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, 
+                looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.
                 </p>
                 <p>
-                  Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de
-                  Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by
-                  Cicero, written in 45 BC. This book is a treatise on the
-                  theory of ethics, very popular during the Renaissance.
+                Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et 
+                Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance.
                 </p>
               </div>
               <div className="col-lg-6">
                 <div className="content ps-0 ps-lg-5">
                   <p>
-                    There are many variations of passages of Lorem Ipsum
-                    available, but the majority have suffered alteration in some
-                    form, by injected humour, or randomised words which don't
-                    look even slightly believable.All the Lorem Ipsum generators
-                    on the Internet tend to repeat predefined chunks as
-                    necessary, making this the first true generator on the
-                    Internet.
+                  There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, 
+                  by injected humour, or randomised words which don't look even slightly believable.All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, 
+                  making this the first true generator on the Internet. 
                   </p>
                   <p>
-                    All the Lorem Ipsum generators on the Internet tend to
-                    repeat predefined chunks as necessary, making this the first
-                    true generator on the Internet. The standard chunk of Lorem
-                    Ipsum used since the 1500s is reproduced below for those
-                    interested.
+                  All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet.
+                  The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested.
                   </p>
                 </div>
               </div>
               <div className="row gy-4">
                 <div className="row gy-5 col-lg-6">
                   <p>
-                    Is a long established fact that a reader will be distracted
-                    by the readable content of a page when looking at its
-                    layout. The point of using Lorem Ipsum is that it has a
-                    more-or-less normal distribution of letters, as opposed to
-                    using 'Content here, content here', making it look like
-                    readable English. Many desktop publishing packages and web
-                    page editors now use Lorem Ipsum as their default model
-                    text, and a search for 'lorem ipsum' will uncover many web
-                    sites still in their infancy.
+                   Is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. 
+                  The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors 
+                  now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy.
                   </p>
                 </div>
                 <div className="row gy-1 col-lg-6">
                   <div className="content ps-0 ps-lg-5">
                     <h3>Lorem Ipsum</h3>
                     <p>
-                      Lorem Ipsum has been the industry's standard dummy text
-                      ever since the 1500s, when an unknown printer took a
-                      galley of type and scrambled it to make a type specimen
-                      book. It has survived not only five centuries, but also
-                      the leap into electronic typesetting, remaining
-                      essentially unchanged.
+                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to 
+                    make a type specimen book. It has survived not only five centuries, 
+                    but also the leap into electronic typesetting, remaining essentially unchanged. 
                     </p>
                   </div>
                 </div>
@@ -125,7 +100,6 @@ export function Sobre() {
           </div>
         </section>
       </main>
-      </div>
     </>
   );
 }
