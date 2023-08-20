9f96ffa08fb413bed341ccdfd999786e71167157
Gustavo Marcelo Penido Pereira <82380592+guhpenido@users.noreply.github.com>
2023-08-20 16:29:41 -0300

    Alteração da página Timeline, tentativa de resolver problema de login. #4

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,31 +1,47 @@
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import arrowImg from "../../assets/arrow.svg";
 import logoImg from "../../assets/logo.png";
-//import "./stylesLogin.css";
-//import { signInWithEmailAndPassword } from "firebase/auth";
 import { app } from "../../services/firebaseConfig";
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  RecaptchaVerifier,
+  onAuthStateChanged,
+} from "firebase/auth";
 import { useNavigate } from "react-router-dom";
-import { getAuth } from "firebase/auth";
-import { signInWithEmailAndPassword } from "firebase/auth";
+
 export function Login() {
+  const auth = getAuth(app);
+  const navigate = useNavigate();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const navigate = useNavigate();
-  const auth = getAuth();
-  function handleSignIn(e) {
-    e.preventDefault();
-    signInWithEmailAndPassword(email, password)
+
+  const recaptchaVerifier = new RecaptchaVerifier("recaptcha-container", {
+    size: "invisible",
+    callback: (response) => {
+      handleSignIn();
+    },
+  });
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        navigate("/timeline");
+      }
+    });
+
+    return () => unsubscribe();
+  }, [auth]);
+
+  function handleSignIn() {
+    signInWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
-        // The user has signed in successfully
         const user = userCredential.user;
         console.log("User logged in:", user);
-
-        // Redirect to a protected route or any other page after successful login
-        navigate("/dm");
+        navigate("/timeline");
       })
       .catch((error) => {
-        // Handle login error
         console.error("Error signing in:", error.message);
       });
   }
@@ -33,6 +49,7 @@ export function Login() {
     <div className="login">
       <div className="container">
         <header className="header">
+          <div id="recaptcha-container"></div>
           <img src={logoImg} alt="CEFERNO" className="logoImg" />
           <span>Por favor digite suas informações de login</span>
         </header>
@@ -59,10 +76,9 @@ export function Login() {
               onChange={(e) => setPassword(e.target.value)}
             />
           </div>
-
           <a href="#">Esqueceu sua senha ?</a>
 
-          <button className="button" onClick={handleSignIn}>
+          <button className="button" id="sign-in-button" onClick={handleSignIn}>
             Entrar <img src={arrowImg} alt="->" />
           </button>
           <div className="footer">
--- src/pages/Timeline/index.jsx
+++ src/pages/Timeline/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faComment } from "@fortawesome/fontawesome-free-solid";
@@ -6,138 +6,283 @@ import { faThumbsUp } from "@fortawesome/fontawesome-free-solid";
 import { faThumbsDown } from "@fortawesome/fontawesome-free-solid";
 import { faCaretDown } from "@fortawesome/fontawesome-free-solid";
 import { faArrowRight } from "@fortawesome/fontawesome-free-solid";
+import { faEnvelope } from "@fortawesome/fontawesome-free-solid";
+//import { faMagnifyingGlassArrowRight } from "@fortawesome/fontawesome-free-solid";
+import { faBell } from "@fortawesome/fontawesome-free-solid";
+import { faQuestion } from "@fortawesome/fontawesome-free-solid";
+import { faPaperPlane } from "@fortawesome/fontawesome-free-solid";
+//import { faXmark } from "@fortawesome/fontawesome-free-solid";
+import { app } from "../../services/firebaseConfig";
+import { getFirestore } from "firebase/firestore";
+import {
+  doc,
+  getDoc,
+  onSnapshot,
+  collection,
+  query,
+  where,
+} from "firebase/firestore";
+
 import "./stylesTimeline.css";
 
 export function Timeline() {
+  const db = getFirestore(app);
+  const [posts, setPosts] = useState([]);
+
+  const getPostsFromFirestore = () => {
+    return onSnapshot(collection(db, "timeline"), (snapshot) => {
+      const postsData = [];
+      snapshot.forEach((doc) => {
+        const postData = {
+          id: doc.id,
+          ...doc.data(),
+        };
+        postsData.push(postData);
+      });
+      setPosts(postsData);
+    });
+  };
+
+  const fetchUserData = async (userId) => {
+    const userDoc = await getDoc(doc(db, "users", userId));
+    if (userDoc.exists()) {
+      return userDoc.data();
+    } else {
+      console.log("User not found");
+      return null;
+    }
+  };
+
+  useEffect(() => {
+    const unsubscribe = getPostsFromFirestore();
+    return () => unsubscribe(); // Cleanup: unsubscribe when component unmounts
+  }, []);
+
+  const [isVisible, setIsVisible] = useState(false);
+
+  const toggleVisibility = () => {
+    setIsVisible(!isVisible);
+  };
+
   return (
-    <div className="tl-container">
-      <div className="tl-header">
-        <div className="tl-header1">
-          <div className="tl-foto">
-            <img
-              src="https://cdn.discordapp.com/attachments/871728576972615680/1142350249776648262/image.png"
-              alt=""
-            />
-          </div>
-          <div className="tl-logo">
-            <img
-              src="https://cdn.discordapp.com/attachments/871728576972615680/1142335297980477480/Ceferno_2.png"
-              alt=""
-            />
-          </div>
-        </div>
-        <div className="tl-titulo">
-          <h1>Timeline</h1>
-        </div>
-      </div>
-      <div className="tl-main">
-        <div className="tl-box">
-          <div className="tl-post">
-            <div className="tl-ps-header">
-              <div className="tl-ps-foto">
+    <>
+      <div className="tl-screen">
+      {isVisible && (
+      <div className="tl-addPost">
+          <div className="tl-addPost-header">
+            <div className="tl-addPost-close" onClick={toggleVisibility}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="1em"
+                viewBox="0 0 384 512"
+              >
+                <path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z" />
+              </svg>
+            </div>
+            <div className="tl-addPost-mode">
+              <div className="tl-addPost-foto">
                 <img
-                  src="https://cdn.discordapp.com/attachments/871728576972615680/1142349089133047868/image.png"
+                  src="https://cdn.discordapp.com/attachments/871728576972615680/1142350249776648262/image.png"
                   alt=""
                 />
               </div>
-              <div className="tl-ps-nomes">
-                <p className="tl-ps-nome">
-                  Stella <span className="tl-ps-user">@tellaswift </span>
-                  <span className="tl-ps-tempo">• 42s</span>
-                  <FontAwesomeIcon className="arrow" icon={faArrowRight} />
-                  <img
-                    src="https://cdn.discordapp.com/attachments/871728576972615680/1142348920949833829/image.png"
-                    alt=""
-                  />{" "}
-                  Kettles{" "}
-                  <span className="tl-ps-userReceived">@eokettles </span>
-                </p>
-              </div>
-            </div>
-            <div className="tl-ps-texto">
-              <p>
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry's standard dummy
-                text ever since the 1500s, when an unknown printer took a galley
-                of type and scrambled it to make a type specimen book. It has
-                survived not only five centuries, but also the leap into
-                electronic typesetting, remaining essentially unchanged. It was
-                popularised in the 1960s with the release of Letraset sheets
-                containing Lorem Ipsum passages, and more recently with desktop
-                publishing software like Aldus PageMaker including versions of
-                Lorem Ipsum.
-              </p>
-            </div>
-            <div className="tl-ps-footer">
-              <div className="tl-ps-opcoes">
-                <div className="tl-ps-reply">
-                  <FontAwesomeIcon icon={faComment} />
-                  <span>10</span>
-                </div>
-                <div className="tl-ps-like">
-                  <FontAwesomeIcon icon={faThumbsUp} /> <span>10</span>
-                </div>
-                <div className="tl-ps-deslike">
-                  <FontAwesomeIcon icon={faThumbsDown} />
-                  <span>10</span>
-                </div>
-              </div>
+              <select className="tl-addPost-modeSelect" name="postMode">
+                <option className="tl-addPost-modeSelect-option" value="normal">
+                  Gustavo Penido
+                </option>
+                <option
+                  className="tl-addPost-modeSelect-option"
+                  value="anonimo"
+                >
+                  novinho 69
+                </option>
+              </select>
+              <FontAwesomeIcon
+                className="tl-addPost-arrow"
+                icon={faArrowRight}
+              />
+              <input
+                className="tl-addPost-mark-input"
+                type="search"
+                value="Timeline"
+              />
             </div>
           </div>
-          <div className="tl-post">
-            <div className="tl-ps-header">
-              <div className="tl-ps-foto">
+          <div className="tl-addpost-body">
+            <div className="tl-textInput">
+              <textarea
+                className="tl-textInput-input"
+                placeholder="O que você deseja susurrar alto hoje?"
+              ></textarea>
+            </div>
+            <div className="tl-confirmPost">
+              <button>
+                <FontAwesomeIcon icon={faPaperPlane} /> Wispar
+              </button>
+            </div>
+          </div>
+        </div>
+        )}
+        <div className="tl-container">
+          <div className="tl-header">
+            <div className="tl-header1">
+              <div className="tl-foto">
                 <img
-                  src="https://cdn.discordapp.com/attachments/871728576972615680/1142349089133047868/image.png"
+                  src="https://cdn.discordapp.com/attachments/871728576972615680/1142350249776648262/image.png"
                   alt=""
                 />
               </div>
-              <div className="tl-ps-nomes">
-                <p className="tl-ps-nome">
-                  Stella <span className="tl-ps-user">@tellaswift </span>
-                  <span className="tl-ps-tempo">• 1h</span>
-                </p>
+              <div className="tl-logo">
+                <img
+                  src="https://cdn.discordapp.com/attachments/871728576972615680/1142335297980477480/Ceferno_2.png"
+                  alt=""
+                />
               </div>
             </div>
-            <div className="tl-ps-texto">Esse é meu primeiro post.</div>
-            <div className="tl-ps-footer">
-              <div className="tl-ps-opcoes">
-                <div className="tl-ps-reply">
-                  <FontAwesomeIcon icon={faComment} />
-                  <span>10</span>
+            <div className="tl-titulo">
+              <h1>Timeline</h1>
+            </div>
+          </div>
+          <div className="tl-main">
+            <div className="tl-box">
+              
+              <div className="tl-post">
+                <div className="tl-ps-header">
+                  <div className="tl-ps-foto">
+                    <img
+                      src="https://cdn.discordapp.com/attachments/871728576972615680/1142349089133047868/image.png"
+                      alt=""
+                    />
+                  </div>
+                  <div className="tl-ps-nomes">
+                    <p className="tl-ps-nome">
+                      Stella <span className="tl-ps-user">@tellaswift </span>
+                      <span className="tl-ps-tempo">• 42s</span>
+                      <FontAwesomeIcon className="arrow" icon={faArrowRight} />
+                      <img
+                        src="https://cdn.discordapp.com/attachments/871728576972615680/1142348920949833829/image.png"
+                        alt=""
+                      />{" "}
+                      Kettles{" "}
+                      <span className="tl-ps-userReceived">@eokettles </span>
+                    </p>
+                  </div>
                 </div>
-                <div className="tl-ps-like">
-                  <FontAwesomeIcon icon={faThumbsUp} /> <span>10</span>
+                <div className="tl-ps-texto">
+                  <p>
+                    Lorem Ipsum is simply dummy text of the printing and
+                    typesetting industry. Lorem Ipsum has been the industry's
+                    standard dummy text ever since the 1500s, when an unknown
+                    printer took a galley of type and scrambled it to make a
+                    type specimen book. It has survived not only five centuries,
+                    but also the leap into electronic typesetting, remaining
+                    essentially unchanged. It was popularised in the 1960s with
+                    the release of Letraset sheets containing Lorem Ipsum
+                    passages, and more recently with desktop publishing software
+                    like Aldus PageMaker including versions of Lorem Ipsum.
+                  </p>
                 </div>
-                <div className="tl-ps-deslike">
-                  <FontAwesomeIcon icon={faThumbsDown} />
-                  <span>10</span>
+                <div className="tl-ps-footer">
+                  <div className="tl-ps-opcoes">
+                    <div className="tl-ps-reply">
+                      <FontAwesomeIcon icon={faComment} />
+                      <span>10</span>
+                    </div>
+                    <div className="tl-ps-like">
+                      <FontAwesomeIcon icon={faThumbsUp} /> <span>10</span>
+                    </div>
+                    <div className="tl-ps-deslike">
+                      <FontAwesomeIcon icon={faThumbsDown} />
+                      <span>10</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="tl-post">
+                <div className="tl-ps-header">
+                  <div className="tl-ps-foto">
+                    <img
+                      src="https://cdn.discordapp.com/attachments/871728576972615680/1142349089133047868/image.png"
+                      alt=""
+                    />
+                  </div>
+                  <div className="tl-ps-nomes">
+                    <p className="tl-ps-nome">
+                      Stella <span className="tl-ps-user">@tellaswift </span>
+                      <span className="tl-ps-tempo">• 1h</span>
+                    </p>
+                  </div>
+                </div>
+                <div className="tl-ps-texto">Esse é meu primeiro post.</div>
+                <div className="tl-ps-footer">
+                  <div className="tl-ps-opcoes">
+                    <div className="tl-ps-reply">
+                      <FontAwesomeIcon icon={faComment} />
+                      <span>10</span>
+                    </div>
+                    <div className="tl-ps-like">
+                      <FontAwesomeIcon icon={faThumbsUp} /> <span>10</span>
+                    </div>
+                    <div className="tl-ps-deslike">
+                      <FontAwesomeIcon icon={faThumbsDown} />
+                      <span>10</span>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
+          <div className="tl-addpost" onClick={toggleVisibility}>
+            <img
+              src="https://cdn.discordapp.com/attachments/871728576972615680/1142352433352294482/asa.png"
+              alt=""
+            />
+          </div>
+          <div className="tl-footer">
+            <div>
+              <FontAwesomeIcon icon={faEnvelope} />
+            </div>
+            <div>
+              <FontAwesomeIcon icon={faBell} />
+            </div>
+            <div>
+              <FontAwesomeIcon icon={faQuestion} />
+            </div>
+            <div>
+              <FontAwesomeIcon icon={faQuestion} />
+            </div>
+          </div>
         </div>
+        {/*<div className="tl-menu">
+          <div className="tl-menu-header">
+            <div className="tl-menu-header-profile">
+              <div className="tl-menu-foto"></div>
+              <div className="tl-menu-nomes"></div>
+              <div className="tl-menu-follows"></div>
+            </div>
+          </div>
+          <div className="tl-menu-body">
+            <div className="tl-menu-options">
+              <div className="tl-menu-option">
+              <a href="">Perfil</a>
+              </div>     
+              <div className="tl-menu-option">
+              <a href="">Whispers privados</a>
+              </div>
+              <div className="tl-menu-option">
+              <a href="">Configurações</a>
+              </div>
+              <div className="tl-menu-option">
+              <a href="">Notificações</a>
+              </div>
+              <div className="tl-menu-option">
+              <a href="">Pesquisar</a>
+              </div>
+            </div>            
+          </div>
+          <div className="tl-menu-footer"></div>
+  </div>*/}
       </div>
-      <div className="tl-addpost">
-        <img
-          src="https://cdn.discordapp.com/attachments/871728576972615680/1142352433352294482/asa.png"
-          alt=""
-        />
-      </div>
-      <div className="tl-footer">
-        <div>
-        <FontAwesomeIcon className="tl-icon-footer" icon={faThumbsDown} />
-        </div>
-        <div>
-          <FontAwesomeIcon className="tl-icon-footer" icon={faThumbsDown} />
-        </div>
-        <div>
-          <FontAwesomeIcon className="tl-icon-footer" icon={faThumbsDown} />
-        </div>
-        <div>
-          <FontAwesomeIcon className="tl-icon-footer" icon={faThumbsDown} />
-        </div>
-      </div>
-    </div>
+    </>
   );
 }
