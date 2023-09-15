e8a31433280ca6abe6dd67273d23eac10c2c10ae
Davi Assunção dos Santos <82240058+davi011205@users.noreply.github.com>
2023-09-15 11:50:58 -0300

    Atualizando a partir da production

--- src/pages/Sobre/index.jsx
+++ src/pages/Sobre/index.jsx
@@ -1,15 +1,8 @@
 import React from "react";
-<<<<<<< HEAD
-
-// import "../bootstrap/css/bootstrap.min.css";
-import "../bootstrap/js/bootstrap.bundle.min.js";
-// import "./stylesSobre.css";
-=======
 /*
 import "../bootstrap/css/bootstrap.min.css";
 import "../bootstrap/js/bootstrap.bundle.min.js";
 import "./stylesSobre.css";*/
->>>>>>> production
 import { Link } from "react-router-dom";
 
 export function Sobre() {
--- src/pages/Timeline/index.jsx
+++ src/pages/Timeline/index.jsx
@@ -42,28 +42,12 @@ import {
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
 
-<<<<<<< HEAD
-import "./stylesTimeline.css";
-import {initializeApp} from "firebase/app"
-import {addDoc, getDocs, updateDoc} from "firebase/firestore"
-
-// configurações do firebase
-const firebaseapp = initializeApp({
-  apiKey: "AIzaSyCWBhfit2xp3cFuIQez3o8m_PRt8Oi17zs",
-  authDomain: "auth-ceferno.firebaseapp.com",
-  projectId: "auth-ceferno",
-  storageBucket: "auth-ceferno.appspot.com",
-  messagingSenderId: "388861107940",
-  appId: "1:388861107940:web:0bf718602145d96cc9d6f1"
-});
-=======
 import PostDisplay from "./post";
 
 import { addDoc } from "firebase/firestore";
 import { CSSTransition, TransitionGroup } from "react-transition-group";
 
 import "./stylesTimeline.css";
->>>>>>> production
 
 export function Timeline() {
   const [selectedUser, setSelectedUser] = useState("");
@@ -88,109 +72,6 @@ export function Timeline() {
   const [hasLoadedPosts, setHasLoadedPosts] = useState(false);
   const [isFetching, setIsFetching] = useState(false);
 
-<<<<<<< HEAD
-
-  // const [selectedOption, setSelectedOption] = useState(null);
-  const [box1Visible, setBox1Visible] = useState(false); //para mostrar a div denuncia conteudo indevido
-  const [box2Visible, setBox2Visible] = useState(false); //para mostrar a div denuncia ser outra pessoa
-  const [h1Visible, setH1Visible] = useState(false); //mostra os h1s que chamam as divs
-  
-  const [denunciaId, setDenunciaId] = useState("");
-  const [messageReportedId, setMessageReportedId] = useState("");
-  const [motive, setMotive] = useState("");
-  const [userReported, setUserReported] = useState("");
-  const [userReporting, setUserReporting] = useState("");
-  const [denuncias, setDenuncias] = useState([]);
-
-  // const db = getFirestore(firebaseapp);
-  const userCollectionRef = collection(db, "denuncia");
-
-  async function CriarUser() {
-    const currentTime = new Date();
-    
-    const user = await addDoc(userCollectionRef, {
-        messageReportedId,
-        motive,
-        time: currentTime.toString(),
-        userReported,
-        userReporting: selectedProfile
-      });
-
-      const newDenunciaId = user.id;
-      await updateDoc(user, { denunciaId: newDenunciaId });
-      console.log(user);
-
-      setH1Visible(!h1Visible);
-      setBox1Visible(false);
-      setBox2Visible(false);
-  }
-  
-  
-
-  useEffect(() => {
-    const getUsers = async () => {
-      const data = await getDocs(userCollectionRef);  
-      setDenuncias(data.docs.map((doc) => ({...doc.data(), id: doc.id})));
-    };
-    getUsers();
-  }, []); 
-
-  //deixa e tira a visibilidade da div denuncia conteudo indevido
-  const toggleBox1Visibility = () => {
-    setBox1Visible(!box1Visible);
-    setBox2Visible(false); // Hide box2 when showing box1
-  };
-
-  //deixa e tira a visibilidade da div denuncia ser outra pessoa
-  const toggleBox2Visibility = () => {
-    setBox2Visible(!box2Visible);
-    setBox1Visible(false); // Hide box1 when showing box2
-  };
-
-  //deixa e tira a visibilidade dos h1s
-  const toggleh1Visibility = () => {
-    setH1Visible(!h1Visible);
-    setBox1Visible(false);
-    setBox2Visible(false);
-  };
-
-  const handleMotiveChange = (event) => {
-    setMotive(event.target.value);
-  };
-  
-  const renderDivStructure = () => {
-    return (
-      <div className={`denuncia ${h1Visible ? 'visible' : 'invisible'}`}> 
-        <h1>Denúncia <button onClick={toggleh1Visibility}>X</button></h1>
-        <label className={`${h1Visible ? 'visible' : 'invisible'}`} htmlFor='box1'> Está publicando conteúdo que não deveria estar no Ceferno  <button className="alternaOpcao" onClick={toggleBox1Visibility}> <img src="src\pages\Timeline\assets\icone.png"/> </button></label>
-        <select className={`opcoesDenuncia box1 ${box1Visible ? 'visible' : 'invisible'}`} id="box1" name="box1" value={motive} onChange={handleMotiveChange}>
-          <option value="Eh_Spam"> É spam </option>
-          <option value="Nao_Gostei"> Simplesmente não gostei </option>
-          <option value="Suicidio_Automutilacao_Disturbios"> Suicidio, automutilação ou disturbios alimentares </option>
-          <option value="Produtos_ilicitos"> Venda de produtos ilicitos </option>
-          <option value="Nudez"> Nudez ou atividade sexual </option>
-          <option value="Discurso_de_Odio"> Símbolos ou discurso de ódio </option>
-          <option value="Violencia"> Violência ou organizações perigosas </option>
-          <option value="Bullying"> Bullying ou assédio </option>
-          <option value="Violacao_Intelectual"> Violação de propriedade intelectual </option>
-          <option value="Golpe"> Golpe ou fraude </option>
-          <option value="Fake_News"> Informação falsa </option>
-        </select>
-        
-        <label className={`${h1Visible ? 'visible' : 'invisible'}`} htmlFor='box2'> Está publicando conteúdo que não deveria estar no Ceferno  <button className="alternaOpcao" onClick={toggleBox2Visibility}> <img src="src\pages\Timeline\assets\icone.png"/> </button></label>       
-         <select className={`opcoesDenuncia box2 ${box2Visible ? 'visible' : 'invisible'}`} id="box2" name="box2" value={motive} onChange={handleMotiveChange}>
-          <option value="fingindo_Ser_Eu"> Eu </option>
-          <option value="fingindo_Ser_Alguem_que_Sigo"> Alguém que sigo </option>
-          <option value="fingindo_Ser_Uma_Celebridade_Figura_Publica"> Uma celebridade ou figura pública </option>
-          <option value="fingindo_Ser_Empresa"> Uma empresa ou organização </option>
-        </select>
-        <button className={`enviar ${h1Visible ? 'visible' : 'invisible'}`} onClick={CriarUser}>Enviar</button>
-
-      </div>
-    );
-  };
-
-=======
   const [nextPostId, setNextPostId] = useState(0);
   const handleScroll = () => {
     const windowHeight = window.innerHeight;
@@ -222,7 +103,6 @@ export function Timeline() {
       });
     }
   }, [isFetching]);
->>>>>>> production
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
       if (user) {
@@ -685,173 +565,6 @@ export function Timeline() {
             </div>
           </div>
           <div className="tl-main">
-<<<<<<< HEAD
-            <div className="tl-box">
-              {/*{posts.map(async (post) => {
-                const userSentData = await fetchUserData(post.userSent);
-                const userMentionedData = await fetchUserData(
-                  post.userMentioned
-                );
-
-                const userSentImage = await fetchUserImage(userSentData);
-                const userMentionedImage = await fetchUserImage(
-                  userMentionedData
-                );
-
-                return (
-                  <div className="tl-box" key={post.id}>
-                    <div className="tl-post">
-                      <div className="tl-ps-header">
-                        <div className="tl-ps-foto">
-                          {userSentImage && <img src={userSentImage} alt="" />}
-                        </div>
-                        <div className="tl-ps-nomes">
-                          <p className="tl-ps-nome">
-                            {userSentData.nome}{" "}
-                            <span className="tl-ps-user">
-                              @{userSentData.usuario}{" "}
-                            </span>
-                            <span className="tl-ps-tempo">• {post.time}</span>
-                            <FontAwesomeIcon
-                              className="arrow"
-                              icon={faArrowRight}
-                            />
-                            {post.userMentioned !== null && (
-                              <div>
-                                {userMentionedImage && (
-                                  <img src={userMentionedImage} alt="" />
-                                )}
-                                {userMentionedData.nome}{" "}
-                                <span className="tl-ps-userReceived">
-                                  @{userMentionedData.usuario}{" "}
-                                </span>
-                              </div>
-                            )}
-                          </p>
-                        </div>
-                      </div>
-                      <div className="tl-ps-texto">
-                        <p>{post.text}</p>
-                      </div>
-                      <div className="tl-ps-footer">
-                        <div className="tl-ps-opcoes">
-                          <div className="tl-ps-reply">
-                            <FontAwesomeIcon icon={faComment} />
-                            <span>{post.replyCount}</span>
-                          </div>
-                          <div className="tl-ps-like">
-                            <FontAwesomeIcon icon={faThumbsUp} />{" "}
-                            <span>{post.likes}</span>
-                          </div>
-                          <div className="tl-ps-deslike">
-                            <FontAwesomeIcon icon={faThumbsDown} />{" "}
-                            <span>{post.deslikes}</span>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                );
-              })}*/}
-
-              <div className="tl-post">
-                <div className="tl-ps-header">
-                  <div className="tl-ps-foto">
-                    <img
-                      src="https://cdn.discordapp.com/attachments/871728576972615680/1142349089133047868/image.png"
-                      alt=""
-                    />
-                  </div>
-                  <div className="tl-ps-nomes">
-                    <p className="tl-ps-nome">
-                      Stella <span className="tl-ps-user">@tellaswift </span>
-                      <span className="tl-ps-tempo">• 42s</span>
-                      <FontAwesomeIcon className="arrow" icon={faArrowRight} />
-                      <img
-                        src="https://cdn.discordapp.com/attachments/871728576972615680/1142348920949833829/image.png"
-                        alt=""
-                      />{" "}
-                      Kettles{" "}
-                      <span className="tl-ps-userReceived">@eokettles </span>
-                    </p>
-                    <div className='iconeDenuncia'>
-                      <button onClick={toggleh1Visibility}> <img src="src\pages\Timeline\assets\alerta.png" alt="" /></button>
-                    </div>
-                    {renderDivStructure()}
-                  </div>
-                </div>
-                <div className="tl-ps-texto">
-                  <p>Ain apelaummmm!</p>
-                </div>
-                <div className="tl-ps-footer">
-                  <div className="tl-ps-opcoes">
-                    <div className="tl-ps-reply">
-                      <FontAwesomeIcon icon={faComment} />
-                      <span>10</span>
-                    </div>
-                    <div className="tl-ps-like">
-                      <FontAwesomeIcon icon={faThumbsUp} /> <span>10</span>
-                    </div>
-                    <div className="tl-ps-deslike">
-                      <FontAwesomeIcon icon={faThumbsDown} />
-                      <span>10</span>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="tl-post">
-                <div className="tl-ps-header">
-                  <div className="tl-ps-foto">
-                    <img
-                      src="https://cdn.discordapp.com/attachments/871728576972615680/1142349089133047868/image.png"
-                      alt=""
-                    />
-                  </div>
-                  <div className="tl-ps-nomes">
-                    <p className="tl-ps-nome">
-                      Stella <span className="tl-ps-user">@tellaswift </span>
-                      <span className="tl-ps-tempo">• 1h</span>
-                    </p>
-                  </div>
-                </div>
-                <div className="tl-ps-texto">Esse é meu primeiro post.</div>
-                <div className="tl-ps-footer">
-                  <div className="tl-ps-opcoes">
-                    <div className="tl-ps-reply">
-                      <FontAwesomeIcon icon={faComment} />
-                      <span>10</span>
-                    </div>
-                    <div className="tl-ps-like">
-                      <FontAwesomeIcon icon={faThumbsUp} /> <span>10</span>
-                    </div>
-                    <div className="tl-ps-deslike">
-                      <FontAwesomeIcon icon={faThumbsDown} />
-                      <span>10</span>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="tl-addpost" onClick={toggleVisibility}>
-            <img
-              src="https://cdn.discordapp.com/attachments/871728576972615680/1142352433352294482/asa.png"
-              alt=""
-            />
-          </div>
-          <div className="tl-footer">
-            <div>
-              <FontAwesomeIcon icon={faEnvelope} />
-            </div>
-            <div>
-              <FontAwesomeIcon icon={faBell} />
-            </div>
-            <div>
-              <FontAwesomeIcon icon={faQuestion} />
-            </div>
-            <div>
-              <FontAwesomeIcon icon={faQuestion} />
-=======
             <div className="tl-container">
               {loadedPosts.map(({ post, userSentData, userMentionedData }) => (
                 <PostDisplay
@@ -861,7 +574,6 @@ export function Timeline() {
                   userMentionedData={userMentionedData}
                 />
               ))}
->>>>>>> production
             </div>
             {isFetching && <p>Carregando mais posts...</p>}
           </div>
