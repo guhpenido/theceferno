4a0cb1686d12ecf15fe584fc6998b4f7f55babcf
Gustavo Marcelo Penido Pereira <82380592+guhpenido@users.noreply.github.com>
2023-10-30 01:43:00 -0300

    .

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -17,6 +17,7 @@ import VisitorPage from "../pages/Perfil/ProfilePage/VisitorPage";
 import SeachPage from "../pages/Perfil/ProfilePage/SearchPage";
 import { Acessibilidade } from "../pages/Acessibilidade/index";
 import Trending from "../pages/Timeline/Trending";
+import Notificacao from "../pages/Notificacao/Notificacao";
 //
 export function AppRoutes() {
   return (
@@ -40,6 +41,7 @@ export function AppRoutes() {
         <Route path="/acc" element={<Acessibilidade />} />
         <Route path="/trending" element={<Trending />} />
         <Route path="/savedPosts" element={<SavedPosts />} />
+        <Route path="/not" element={<Notificacao />} />
       </Routes>
     </BrowserRouter>
   );
